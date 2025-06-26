cb534a20deb21e45c88b7ace44f3f6e20c2aebd2
harun241 <omdharun6@gmail.com>
2025-06-26 18:12:18 +0600

    correction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -159,12 +159,12 @@ const Navbar = ({ toggleTheme, theme }) => {
                 </NavLink>
               </>
             )}
-            <button
+            <small><button
               onClick={toggleTheme}
               className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
             >
               {theme === "light" ? "Dark" : "Light"} Mode
-            </button>
+            </button></small>
             {!user ? (
               <NavLink to="/login" onClick={() => setMobileMenuOpen(false)} className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">
                 Login
